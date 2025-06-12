bda2b38cf642d7ac8d61ac5de9b5ca5ae8452606
Henrique <151760683+Sorrateiro@users.noreply.github.com>
2025-06-12 00:25:27 -0300

    calculando as seguidas

--- scripts/scriptsdosite.js
+++ scripts/scriptsdosite.js
@@ -1,18 +1,4 @@
-const traducoes = {
-        "pt": {
-            "textextodocorpoto":getElementById("noticia1").textContent,
-        },
-        "en": {
-            "textodocorpo": getElementById("noticia1").textContent,
-        },
-        "es": {
-            "textodocorpo": getElementById("noticia1").textContent,
-        }
-    };
-
-    document.getElementById("language-select").addEventListener("change", function() {
-        const idiomaSelecionado = this.value;
-        document.getElementById("textodocorpo").textContent = traducoes[idiomaSelecionado]["textodocorpo"];
-    });
-
-    
+    function leiaMais() {
+        let escreva=document.getElementById("descricao-noticia1");
+        escreva.innerHTML="tudo o que se pode dizer";
+    }
