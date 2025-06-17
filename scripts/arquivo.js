67d10e192963d4c04232de2728f4379471bea39a
Henrique <151760683+Sorrateiro@users.noreply.github.com>
2025-06-16 21:43:43 -0300

    atualização 3

---
+++ scripts/arquivo.js
@@ -0,0 +1,6 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F8') {
+        alert('Você apertou F8!');
+        // Aqui você pode adicionar qualquer ação que deseja executar ao pressionar F8
+    }
+});
--- scripts/scriptsdosite.js
+++ scripts/scriptsdosite.js
@@ -1,4 +1,19 @@
-    function leiaMais() {
-        let escreva=document.getElementById("descricao-noticia1");
-        escreva.innerHTML="tudo o que se pode dizer";
+function clicou() {
+    var noticia = document.getElementById("noticia");
+    var botao= document.getElementById("botao");
+    if (noticia.style.display === "none") {
+        noticia.style.display = "block";
+        botao.innerHTML = "Esconder Notícia";
+    } else {
+        noticia.style.display = "none";
+        botao.innerHTML = "Mostrar Notícia";
     }
+    
+}
+
+function carregar() {
+    var noticia = document.getElementById("noticia");
+    var botao = document.getElementById("botao");
+    noticia.style.display = "none";
+    botao.innerHTML = "Mostrar Notícia";
+}
